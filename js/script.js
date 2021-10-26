03d9f99c34e7a73e75e8ba94faa5ae0de01463aa
Eshrak Rahman <eshrakurrahman@gmail.com>
2021-10-26 22:09:12 +0600

    adding quotes dynamically
    logic develop to get quotes dynamically and font size depending on the quotes text.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
+// query selector
+const quotesTextElm = document.getElementById("quote");
+const quoteAuthorElm = document.getElementById("author");
+const newQuoteBtnElm = document.getElementById("new-quote");
+const twitterBtnElm = document.getElementsByClassName("twitter-button");
+
+
+
+
 let apiQuotes = [];
+
 // getting different quotes
 function newQuotes() {
     // pick a random quotes from apiQuotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // checking if author name is null then replace it with unknown
+    if (quote.author === null){
+        quoteAuthorElm.textContent = "Unknown";
+    } else {
+        quoteAuthorElm.textContent = quote.author;
+    }
+    // if the text length of quotes bigger then resize the font size
+    if (quote.text.length > 120){
+        quotesTextElm.classList.add('long-quote');
+    }else {
+        quotesTextElm.classList.remove('long-quote');
+    }
+    quotesTextElm.textContent = quote.text;
 }
 
 // get Quotes from api
